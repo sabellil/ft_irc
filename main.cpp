2220e7a95ec3afdd6b201686f879dffcc337a91d
Mailys RIVIERE <mailys.riviere@gmail.com>
2026-02-25 17:14:35 +0100

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/25 16:51:38 by mairivie          #+#    #+#             */
+/*   Updated: 2026/02/25 16:57:19 by mairivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <csignal>
+#include <iostream>
+#include <string>
+
+int main(int ac, char * av) {
+    try {
+        std::cout << av[0] << " <av[0] | ac> " << ac << std::endl;
+        //check argc argv
+        //init signal
+        //init server
+        //run server
+    }
+    // catch {
+    //     //error
+    // }
+}
