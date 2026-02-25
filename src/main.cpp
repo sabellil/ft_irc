7380a51f460bc852be521d72bf3cfe5fddc74831
Mailys RIVIERE <mailys.riviere@gmail.com>
2026-02-25 19:37:41 +0100

    handle SIGINT and SIGQUIT

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,20 @@
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 16:51:38 by mairivie          #+#    #+#             */
-/*   Updated: 2026/02/25 18:13:13 by mairivie         ###   ########.fr       */
+/*   Updated: 2026/02/25 19:31:36 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <csignal>
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <exception>
+
+void    sigStopHandler(int signum) {
+    std::cout << "Interruption du serveur par signal " << signum << std::endl;
+    std::exit(signum); // exit pas dans les fonctions allowed, TODO: une sorte de ft clean close
+}
 
 void    check_arg(int ac, char **av) {
         if (ac != 3)
@@ -21,14 +27,16 @@ void    check_arg(int ac, char **av) {
         for (int i=0; i<ac; i++) {
             std::cout << "av[" << i << "] : " << av[i] << std::endl;
         }
-        //verif port, sinon trow
-        //option check secure password
+        //TODO: 
+        //pour verif port, est-ce qu'une fonction existe pour test ?
+        //option check secure password (bif bof la motiv)
 }
 
 int main(int ac, char **av) {
     try {
         check_arg(ac, av);
-        //init signal
+        signal(SIGINT, *sigStopHandler); // ctrl + c
+        signal(SIGQUIT, *sigStopHandler); //ctrl + \
         //init server
         //run server
         while(1){
