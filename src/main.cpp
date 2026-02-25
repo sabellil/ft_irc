0edd7a8292363fa30003be5b9a992f4b9ab0b839
Mailys RIVIERE <mailys.riviere@gmail.com>
2026-02-25 18:14:41 +0100

    add check nb arg, add exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,36 @@
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 16:51:38 by mairivie          #+#    #+#             */
-/*   Updated: 2026/02/25 17:18:52 by mairivie         ###   ########.fr       */
+/*   Updated: 2026/02/25 18:13:13 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <csignal>
 #include <iostream>
 #include <string>
+#include <exception>
+
+void    check_arg(int ac, char **av) {
+        if (ac != 3)
+            throw std::logic_error("Wrong nbr of arguments. Use ./irc <port> <password>\n");
+        for (int i=0; i<ac; i++) {
+            std::cout << "av[" << i << "] : " << av[i] << std::endl;
+        }
+        //verif port, sinon trow
+        //option check secure password
+}
 
 int main(int ac, char **av) {
-    // try {
-        std::cout << av[0] << " <av[0] | ac> " << ac << std::endl;
-        //check argc argv
+    try {
+        check_arg(ac, av);
         //init signal
         //init server
         //run server
-    // }
-    // catch {
-    //     //error
-    // }
+        while(1){
+            
+        }
+    }
+    catch(const std::logic_error & error) {
+        std::cout << "Caught: " << error.what();
+    }
 }
