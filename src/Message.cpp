c95c0cc0d032be9ed1a545a5b16d492b80663722
sabellil <sabellil@student.42.fr>
2026-03-04 16:06:08 +0100

    Start of parse function TODO: parse params

--- src/Message.cpp
+++ src/Message.cpp
@@ -7,3 +7,35 @@ Message::Message() : sender(0)
 Message::~Message()
 {
 }
+
+
+bool Message::parse(const std::string& line)
+{
+    clear();//TODO : vider tous les elements de Message pour repartir de 0
+    _rawLine = line;
+
+    size_t i = 0;
+    skipSpaces(line, i);//TODO, jump les eventuels espaces au debut du msg
+
+    if (i >= line.size())
+        return false;
+
+    size_t start = i;//command
+    while (i < line.size() && line[i] != ' ')
+        ++i;
+    _command = line.substr(start, i - start);
+    skipSpaces(line, i);
+
+    while (i < line.size())//trailing
+    {
+        if (line[i] == ":")
+        {
+            _trailing = line.substr(i + 1);
+        }
+    }
+
+    //params a recuperer ici 
+
+
+
+}
