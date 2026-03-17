b5ee560414afa65b04c4d72f9c0a5077b6a1226f
sabellil <sabellil@student.42.fr>
2026-03-17 16:27:00 +0100

    Added Channel.cpp and first functions

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -15,4 +15,24 @@ Channel::~Channel()
 {
 }
 
+const std::string& Channel::getName() const
+{
+    return _name;
+}
 
+bool Channel::hasUser(User* user) const
+{
+    return _users.count(user) > 0;
+}
+
+void Channel::addUser(*User user)
+{
+    _users.insert(user);
+}
+
+void Channel::removeUser(*User user)
+{
+    _users.erase(user);
+}
+
+//getuser
